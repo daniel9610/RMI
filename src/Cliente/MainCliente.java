c83fe80d97cce9b94b2a07e7d5b25d6468dc383f
Daniel Torres <stringdaniel@outlook.com>
2019-11-07 21:18:09 -0500

    parte cliente

--- src/Cliente/MainCliente.java
+++ src/Cliente/MainCliente.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package Cliente;
-
+import RMI.RemoteInterface;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import javax.swing.JOptionPane;
 /**
  *
  * @author BOG-A410-E-016
@@ -15,7 +18,21 @@ public class MainCliente {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            String valora = JOptionPane.showInputDialog("Ingrese el valor del numero Uno");
+            String valorb = JOptionPane.showInputDialog("Ingrese el valor del numero Dos");
+            int a = Integer.parseInt(valora);
+            int b = Integer.parseInt(valorb);
+            Registry miRegistro = LocateRegistry.getRegistry("127.0.0.1",1234);
+            RemoteInterface s = (RemoteInterface) miRegistro.lookup("Matematicas");
+            JOptionPane.showMessageDialog(null,"Resultado suma : "+s.suma(a,b));
+            JOptionPane.showMessageDialog(null,"Resultado resta : "+s.resta(a,b));
+            JOptionPane.showMessageDialog(null,"Resultado multiplicacion : "+s.multiplica(a,b));
+            JOptionPane.showMessageDialog(null,"Resultado division : "+s.division(a,b));            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
     
 }
